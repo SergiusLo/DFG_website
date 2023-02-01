9bac31a0e69ea1e89997bcc5031844a37e88cd00
SergiusLo <loboyko99911@gmail.com>
2023-02-01 16:26:48 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      main: ["Inter", "sans-serif"],
+    },
+    colors: {
+      "main-color": "#212121",
+      "second-color": "#2979FF",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
