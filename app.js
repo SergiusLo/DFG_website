6d25e10f87597a7c759a5858d23ee54219d4c904
SergiusLo <loboyko99911@gmail.com>
2023-02-08 00:33:02 +0200

    'adaptive-menu'

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const splider = document.querySelector(".splide");
+
+  let splide = new Splide(splider, {
+    perPage: 4,
+    gap: "1rem",
+    arrowPath: "none",
+    pagination: false,
+    autoScroll: {
+      rewind: true,
+      speed: 1,
+      pauseOnHover: true,
+    },
+    640: {
+      perPage: 3,
+    },
+  });
+  splide.mount(window.splide.Extensions);
+
   const accordion = document.getElementsByClassName("contain");
 
   for (i = 0; i < accordion.length; i++) {
@@ -6,4 +24,16 @@ document.addEventListener("DOMContentLoaded", function () {
       this.classList.toggle("active");
     });
   }
+
+  const btn = document.querySelector(".menu-btn");
+  const btn1 = document.querySelector(".menu");
+  const body = document.querySelector("body");
+  const navig = document.querySelector(".navig");
+
+  btn.addEventListener("click", function () {
+    btn1.classList.toggle("menu--active");
+    btn.classList.toggle("btn--active");
+    body.classList.toggle("hid");
+  });
+
 });
